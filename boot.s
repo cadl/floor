f5c8c23e797c7957ecefd86a46497700cb1d1671
cadl <ctrlaltdeleteliu@gmail.com>
2013-11-16 13:22:11 +0800

    add fat16 head in floopy img

--- boot.s
+++ boot.s
@@ -1,6 +1,28 @@
 .code16
     .section .text
-    .global _start
+    
+jmp _start
+nop
+
+BS_OEMName:         .ascii  "wondwall"
+BPB_BytsPerSec:     .2byte  512
+BPB_SecPerCluster:  .byte   1
+BPB_ResvdSecCnt:    .2byte  1
+BPB_NumFATs:        .byte   2
+BPB_RootEntCnt:     .2byte  224
+BPB_TotSec16:       .2byte  2880
+BPB_Media:          .byte   0xf0
+BPB_FATSz16:        .2byte  9
+BPB_SecPerTrk:      .2byte  18
+BPB_NumHeads:       .2byte  2
+BPB_HiddSec:        .4byte  0
+BPB_TotSec32:       .4byte  0
+BS_DrvNum:          .byte   0
+BS_Reserved1:       .byte   0
+BS_BootSig:         .byte   0x29
+BS_VolID:           .4byte  0
+BS_VolLab:          .ascii  "floor   0.1"
+BS_FileSysType:     .ascii  "FAT12   "
 
 _start:
     mov %cs, %ax
