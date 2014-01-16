45344c9e9b7c0c5a3e1dfc73b558b1588eafa91d
cadl <ctrlaltdeleteliu@gmail.com>
2014-01-16 22:13:42 +0800

    emacs dir-local vars

---
+++ .dir-locals.el
@@ -0,0 +1,16 @@
+((c-mode . ((flycheck-clang-include-path . (
+                                            "."
+                                            "./include"
+                                            "../include"
+                                            "../../include"
+                                            ))
+            
+           (ac-clang-flags . (
+                              "-I/usr/include"
+                              "-I/usr/include/linux"
+                              "-I/usr/local/include"
+                              "-I/usr/lib/gcc/x86_64-redhat-linux/4.8.2/include"
+                              "-I/home/cadl/workspace/opensource/floor/src/include"
+                              ))
+           )
+))
