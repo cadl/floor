7608006914ee6a7d3ddb5f050b26dc0433c93439
cadl <ctrlaltdeleteliu@gmail.com>
2015-04-26 20:45:08 +0800

    Add .clang_complete

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,14 +3,4 @@
                                             "./include"
                                             "../include"
                                             "../../include"
-                                            ))
-            
-           (ac-clang-flags . (
-                              "-I/usr/include"
-                              "-I/usr/include/linux"
-                              "-I/usr/local/include"
-                              "-I/usr/lib/gcc/x86_64-redhat-linux/4.8.2/include"
-                              "-I/home/cadl/workspace/opensource/floor/src/include"
-                              ))
-           )
-))
+                                            )))))
